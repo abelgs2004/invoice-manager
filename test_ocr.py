93853b632f526b5728e0f4a949f3b0491f7b9595
abelgs2004 <abelsubin254@gmail.com>
2026-01-12 00:19:07 +0530

    Cleanup: Remove temporary files and test scripts

--- test_ocr.py
+++
@@ -1,9 +0,0 @@
-
-try:
-    import pypdfium2 as pdfium
-    print("pypdfium2 imported successfully")
-except ImportError:
-    print("pypdfium2 NOT installed")
-
-from backend.services.ocr_service import ocr_pdf
-print("Imported ocr_pdf function")
